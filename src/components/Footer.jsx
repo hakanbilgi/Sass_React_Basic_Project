94200cd056011a7c6a00de49fe0384fe0ae1cae0
Hakan Bilgi <hknblg34@gmail.com>
2022-10-11 15:25:41 +0300

    footer jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import 
+import FooterStyle from "../scss/footer.module.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={FooterStyle.footer}>
+      <p className={FooterStyle.title}>
+        Copyright by Clarusway {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
